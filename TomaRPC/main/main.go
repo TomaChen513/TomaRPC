e04c35585d0089f7ead78d18d082b5faabf7e1fc
TomaChen513 <xy_chen513@163.com>
2023-03-15 23:12:59 +0800

    Add http protocol support

--- TomaRPC/main/main.go
+++ TomaRPC/main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 	"rpc"
 	"sync"
 	"time"
@@ -17,24 +19,29 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
+func startServer(addrCh chan string) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":9999")
+	_ = rpc.Register(&foo)
+	rpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
+}
 
-	client, _ := rpc.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := rpc.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	var wg sync.WaitGroup
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
@@ -43,17 +50,9 @@ func main() {
 	wg.Wait()
 }
 
-func startServer(addr chan string) {
-	var foo Foo
-	if err := rpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
-
-	listener, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", listener.Addr())
-	addr <- listener.Addr().String()
-	rpc.Accept(listener)
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
