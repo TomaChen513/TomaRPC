071f61cef6b9725507f828198c576c862a5c962f
TomaChen513 <1209805607@qq.com>
2023-03-27 17:23:37 +0800

    update

--- codingSummary/main.go
+++ codingSummary/main.go
@@ -1,19 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "strconv"
+	"strings"
+)
 
 func main() {
-	var str string
-	var num int
-	var float float64
-	fmt.Scanf("%d",&num)
-	fmt.Scanf("%s",&str)
-	fmt.Scanf("%f",&float)
+	// var str string
+	// var num int
+	// var float float64
+	// fmt.Scanf("%d",&num)
+	// fmt.Scanf("%s",&str)
+	// fmt.Scanf("%f",&float)
 
 
+	// n
+	// 123 213 213
 	
-	fmt.Println(str)
-	fmt.Println(num)
-	fmt.Println(float)
+	// fmt.Println(str)
+	// fmt.Println(num)
+	// fmt.Println(float)
+	inputDemo()
 	
+}
+
+func inputDemo(){
+	// EgTn61O2Hi
+	var n int
+	fmt.Scan(&n)
+	var line string
+	fmt.Scan(&line)
+	res:=strings.Split(line," ")
+	fmt.Println(res)
 }
