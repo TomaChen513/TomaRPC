46a78babdbb13caa08cf0452fffc6bff46e6f2b3
TomaChen513 <xy_chen513@163.com>
2023-04-02 20:09:22 +0800

    feat: main

--- codingSummary/main.go
+++ codingSummary/main.go
@@ -2,35 +2,98 @@ package main
 
 import (
 	"fmt"
-	// "strconv"
-	"strings"
 )
 
-func main() {
-	// var str string
-	// var num int
-	// var float float64
-	// fmt.Scanf("%d",&num)
-	// fmt.Scanf("%s",&str)
-	// fmt.Scanf("%f",&float)
-
-
-	// n
-	// 123 213 213
-	
-	// fmt.Println(str)
-	// fmt.Println(num)
-	// fmt.Println(float)
-	inputDemo()
-	
+type Node struct {
+	id          int
+	color       int
+	left, right *Node
 }
 
-func inputDemo(){
-	// EgTn61O2Hi
+func main() {
+	// a^b
 	var n int
 	fmt.Scan(&n)
-	var line string
-	fmt.Scan(&line)
-	res:=strings.Split(line," ")
-	fmt.Println(res)
+	nodes := make([]Node, n)
+	// init
+	for i := 0; i < n; i++ {
+		nodes[i] = Node{
+			id:    i + 1,
+			color: -1,
+			left:  nil,
+			right: nil,
+		}
+	}
+
+	// build trees
+	for i := 0; i < n-1; i++ {
+		var parentIndex int
+		fmt.Scan(&parentIndex)
+		parentIndex -= 1
+		sonIndex := i + 1
+		// insert
+		if nodes[parentIndex].left == nil {
+			nodes[parentIndex].left = &nodes[sonIndex]
+		} else {
+			nodes[parentIndex].right = &nodes[sonIndex]
+		}
+	}
+	// get color
+	for i := 0; i < n; i++ {
+		var color int
+		fmt.Scan(&color)
+		nodes[i].color = color
+	}
+
+	var dfs func(node *Node) int
+	dfs = func(node *Node) int {
+		if node.left == nil && node.right == nil {
+			return 1
+		}
+		if node.color == 1 {
+			return dfs(node.left) + dfs(node.right)
+		} else {
+			return dfs(node.left) ^ dfs(node.right)
+		}
+	}
+	fmt.Println(dfs(&nodes[0]))
+}
+
+func xor(a, b int) int {
+	return a ^ b
+}
+func add(a, b int) int {
+	return a + b
+}
+
+func convertToFloat1(s string) string {
+	// find .
+	sb := []byte(s)
+	pointIndex := -1
+	for i := len(sb) - 1; i >= 0; i-- {
+		if sb[i] == '.' {
+			pointIndex = i
+			break
+		}
+	}
+	if pointIndex == -1 {
+		sb = append(sb, '.')
+		sb = append(sb, '0')
+	} else {
+		if pointIndex < len(sb)-2 {
+			if sb[pointIndex+2] >= '5' {
+				sb[pointIndex+1] += 1
+			}
+		}
+		sb = sb[:pointIndex+2]
+	}
+
+	return string(sb)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
